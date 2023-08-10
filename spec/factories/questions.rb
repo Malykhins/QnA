0ec77d914feeec0ec67b0065630c5b8c21487fe8
Malykhins <malykhins@mail.ru>
2023-08-10 23:02:22 +0300

    add tests for QuestionsController, add actions to this controller, create stubs instead of views

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,6 +1,11 @@
 FactoryBot.define do
   factory :question do
+    sequence(:id) { |n| n }
     title { "MyString" }
     body { "MyText" }
+
+    trait :invalid do
+      title { nil }
+    end
   end
 end
