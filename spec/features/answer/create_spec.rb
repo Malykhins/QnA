968dc601c889528cf478378dcfce0ba73efb1182
Malykhins <malykhins@mail.ru>
2023-10-08 16:31:47 +0300

    add story "User can add one or more files when creating an answer"

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -31,11 +31,23 @@ feature 'User can create answer', "
       end
     end
 
-
     scenario 'Answer the question with errors' do
       click_on 'Create Answer'
 
       expect(page).to have_content "Body can't be blank"
+    end
+
+    scenario 'Answer the question with attached files' do
+      within('.new-answer-form') do
+        fill_in 'Body', with: 'Text of answer'
+
+        attach_file 'File', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+
+        click_on 'Create Answer'
+      end
+
+      expect(page).to have_link 'rails_helper.rb'
+      expect(page).to have_link 'spec_helper.rb'
     end
   end
 
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -6,5 +6,9 @@ RSpec.describe Answer, type: :model do
   it { should belong_to(:question).class_name('Question') }
 
   it { should belong_to(:user).class_name('User') }
+
+  it 'have many attached file' do
+    expect(Answer.new.files).to be_an_instance_of(ActiveStorage::Attached::Many)
+  end
 end
 
